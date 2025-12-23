69cc419cd36eb3fb6cfbba71364d2034647bebc7
lay longhav <laylonghav2023@gmail.com>
2025-12-23 20:07:54 +0700

    first commit env3

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,12 +1,21 @@
-require("dotenv").config(); // Load .env variables
 const admin = require("firebase-admin");
 
 admin.initializeApp({
   credential: admin.credential.cert({
+    type: process.env.FIREBASE_TYPE,
     projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
     privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    clientId: process.env.FIREBASE_CLIENT_ID,
+    authUri: process.env.FIREBASE_AUTH_URI,
+    tokenUri: process.env.FIREBASE_TOKEN_URI,
+    authProviderX509CertUrl: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+    clientX509CertUrl: process.env.FIREBASE_CLIENT_CERT_URL,
+    universeDomain: process.env.FIREBASE_UNIVERSE_DOMAIN,
   }),
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 });
 
 const db = admin.firestore();
